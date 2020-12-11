620307ba5592f2bc2d0420a798c007bbc7e0222d
Anderson Rodrigues Gomes <argomes777@gmail.com>
2020-12-11 08:58:10 -0300

    primeiro programa em flutter

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:projeto_perguntas/questionario.dart';
+import 'package:projeto_perguntas/resultado.dart';
+
+main() {
+  runApp(new PerguntaApp());
+}
+
+class PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
+
+  final List<Map<String, Object>> _perguntas = const [
+    {
+      'texto': 'Qual a sua cor favorita?',
+      'respostas': [
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Vermelho', 'pontuacao': 5},
+        {'texto': 'Verde', 'pontuacao': 3},
+        {'texto': 'Branco', 'pontuacao': 1}
+      ]
+    },
+    {
+      'texto': 'Qual o seu animal favorito?',
+      'respostas': [
+        {'texto': 'Coelho', 'pontuacao': 10},
+        {'texto': 'Cobra', 'pontuacao': 5},
+        {'texto': 'Elefante', 'pontuacao': 3},
+        {'texto': 'Leão', 'pontuacao': 1}
+      ]
+    },
+    {
+      'texto': 'Qual o seu instrutor favorito?',
+      'respostas': [
+        {'texto': 'João', 'pontuacao': 10},
+        {'texto': 'Maria', 'pontuacao': 5},
+        {'texto': 'Pedro', 'pontuacao': 3},
+        {'texto': 'Leo', 'pontuacao': 1}
+      ]
+    }
+  ];
+
+  void _responder(int pontuacao) {
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
+      });
+    }
+  }
+
+  void _reiniciarQuestionario() {
+    _perguntaSelecionada = 0;
+    _pontuacaoTotal = 0;
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: Text('Perguntas')),
+            body: temPerguntaSelecionada
+                ? Questionario(
+                    perguntas: _perguntas,
+                    perguntaSelecionada: _perguntaSelecionada,
+                    responder: _responder,
+                  )
+                : Resultado(_pontuacaoTotal, _reiniciarQuestionario)));
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  @override
+  PerguntaAppState createState() {
+    return PerguntaAppState();
+  }
+}
---
+++ lib/questao.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class Questao extends StatelessWidget {
+  final String texto;
+
+  Questao(this.texto);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.all(30),
+        child: Text(texto,
+            style: TextStyle(fontSize: 28), textAlign: TextAlign.center));
+  }
+}
